0b7b60e23cbd0ab702a6a4d6656bf5272eeb5983
Ewan Sheldon <ewansheldon@gmail.com>
2016-10-18 11:23:37 +0100

    account can add and deduct money to and from balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,30 @@
-class Account
+class Account #controls, knows and shows balance
 
-  def initialize
+  def initialize(transactions = TransactionHistory.new)
     @balance = 0
+    @transactions = transactions
   end
 
   def show_balance
     @balance
   end
+
+  def withdraw(amount)
+    deduct(amount)
+  end
+
+  def deposit(amount)
+    add(amount)
+  end
+
+  private
+
+  def deduct(amount)
+  @balance -= amount
+  end
+
+  def add(amount)
+    @balance += amount
+  end
+
 end
---
+++ spec/transaction_history_spec.rb
@@ -0,0 +1,8 @@
+require 'transaction_history'
+
+describe TransactionHistory do
+
+  it 'records a history of all journeys' do
+
+  end
+end
