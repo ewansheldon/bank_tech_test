cc55d30ea2f0d8e8ed8427ceac032737bd9d96ce
Ewan Sheldon <ewansheldon@gmail.com>
2016-10-18 12:12:18 +0100

    account instantiates transactions and saves them to transaction history

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,13 @@
+require_relative 'transaction_history'
+require_relative 'transaction'
+
 class Account #controls, knows and shows balance
 
-  def initialize(transaction_history = TransactionHistory.new)
+  def initialize(transaction_history = TransactionHistory.new,
+                 transaction_class = Transaction)
     @balance = 0
     @transaction_history = transaction_history
-    # @transaction_class = transaction_class
+    @transaction_class = transaction_class
   end
 
   def show_balance
@@ -12,18 +16,20 @@ class Account #controls, knows and shows balance
 
   def withdraw(amount)
     deduct(amount)
-    # @transaction = transaction_class.new
-    # @transaction_history.save
+    @transaction = @transaction_class.new(amount)
+    @transaction_history.save(@transaction, @balance)
   end
 
   def deposit(amount)
     add(amount)
+    @transaction = @transaction_class.new(amount)
+    @transaction_history.save(@transaction, @balance)
   end
 
   private
 
   def deduct(amount)
-  @balance -= amount
+    @balance -= amount
   end
 
   def add(amount)
